f6a3262caddc162015d66fd61ba6caaa53bd4a94
cjcq <425430798@qq.com>
2022-03-30 15:58:21 +0800

    演示workqueues的手动应答，以及解决当一个消费者中断时，由其他消费者处理，保证消息不丢失

---
+++ src/main/java/util/SleepUtils.java
@@ -0,0 +1,10 @@
+package util;
+
+public class SleepUtils {
+    public static void sleep(int second){try {
+        Thread.sleep(1000*second);
+    } catch (InterruptedException _ignored) {
+        Thread.currentThread().interrupt();
+    }
+    }
+}
--- src/main/java/workqueue/WorkConsumer2.java
+++ src/main/java/workqueue/WorkConsumer2.java
@@ -4,6 +4,7 @@ import com.rabbitmq.client.CancelCallback;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DeliverCallback;
 import util.RabbitMqUtils;
+import util.SleepUtils;
 
 
 public class WorkConsumer2 {
@@ -11,13 +12,23 @@ public class WorkConsumer2 {
     public static void main(String[] args) throws Exception {
         Channel channel = RabbitMqUtils.getChannel();
 
-        DeliverCallback deliverCallback=(consumerTag, delivery)->{
-            String receivedMessage = new String(delivery.getBody());
-            System.out.println("接收到消息:"+receivedMessage);
+        //消息消费的时候如何处理消息
+        DeliverCallback deliverCallback=(consumerTag,delivery)->{
+            String message= new String(delivery.getBody());
+            SleepUtils.sleep(30);
+            System.out.println("C2接收到消息:"+message);
+            /**
+             * 1.消息标记 tag
+             * 2.是否批量应答未应答消息
+             */
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(),false);
         };
+        //采用手动应答
+        boolean autoAck=false;
         CancelCallback cancelCallback=(consumerTag)->{
-            System.out.println(consumerTag+"消费者取消消费接口回调逻辑");};
-        System.out.println("C1 消费者启动等待消费......");
-        channel.basicConsume(QUEUE_NAME,true,deliverCallback,cancelCallback);
+            System.out.println(consumerTag+"C2消费者取消消费接口回调逻辑");};
+
+        System.out.println("C2 等待接收消息处理时间较长");
+        channel.basicConsume(QUEUE_NAME,autoAck,deliverCallback,cancelCallback);
     }
 }
